de40a5b1ebf7ecc2642f59bfaed8b0965b1d3c92
LanderPerez17 <dam3.lander.perez@gmail.com>
2022-05-13 14:14:32 +0200

    MostrarInfoObjetos feo

--- JuegoPFC/Assets/Scripts/Inventory/MostrarInfoObjecto.cs
+++ JuegoPFC/Assets/Scripts/Inventory/MostrarInfoObjecto.cs
@@ -8,27 +8,42 @@ public class MostrarInfoObjecto : MonoBehaviour, IPointerEnterHandler, IPointerE
     public string Tipo;
     public string Nombre;
     public string Descripcion;
-    public Text TextTipo;
-    public Text TextNombre;
-    public Text TextDescripcion;
-    public GameObject panel;
+    private Text TextTipo;
+    private Text TextNombre;
+    private Text TextDescripcion;
+    private GameObject panel;
 
     private void Start() {
-        TextTipo.text = Tipo;
-        TextNombre.text = Nombre;
-        TextDescripcion.text = Descripcion;
+        panel = GameObject.Find("PanelInfoObject");
+        TextNombre = panel.transform.GetChild(0).GetComponent<Text>();
+        TextTipo = panel.transform.GetChild(1).GetComponent<Text>();
+        TextDescripcion = panel.transform.GetChild(2).GetComponent<Text>();
+        // TextTipo.text = Tipo;
+        // TextNombre.text = Nombre;
+        // TextDescripcion.text = Descripcion;
     }
 
     public void OnPointerEnter (PointerEventData eventData) 
 	{
 		Debug.Log ("The cursor entered the selectable UI element.");
-        panel.SetActive(true);
+        // panel.SetActive(true);
+        TextTipo.text = Tipo;
+        TextNombre.text = Nombre;
+        TextDescripcion.text = Descripcion;
+        TextTipo.enabled = true;
+        TextNombre.enabled = true;
+        TextDescripcion.enabled = true;
+        panel.GetComponent<Image>().enabled = true;
 	}
 
     public void OnPointerExit(PointerEventData eventData)
     {
         Debug.Log("The cursor exited the selectable UI element.");
-        panel.SetActive(false);
+        // panel.SetActive(false);
+        panel.GetComponent<Image>().enabled = false;
+        TextTipo.enabled = false;
+        TextNombre.enabled = false;
+        TextDescripcion.enabled = false;
     }
     
 }
